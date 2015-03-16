401e853b491055e78d934d6674cdfcc8455ef22e
julien <alternative451@gmail.com>
2015-03-16 23:54:44 +0100

    autocomplete test

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,13 @@
+
 $(document).ready(function() {
     $('#search').autocomplete({
-        source: ['motor', 'esc']
-    })
+
+        source: [{label : "aa", value : "oo", cat : "cat ?"}]
+
+    }).data("ui-autocomplete")._renderItem = function( ul, item ) {
+        return $( "<li>" )
+            .attr( "data-value", item.value )
+            .append( item.label + item.cat + "<<<")
+            .appendTo( ul );
+    }
 });
